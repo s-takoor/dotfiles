70b228055cb60e2346e19e008a2ebce24ffbe131
s-takoor <shashiduth.takoor@gmail.com>
2023-11-21 19:43:00 +0400

    Minor updates to fish functions

--- macos/config/fish/aliases/doom.fish
+++
@@ -1,9 +0,0 @@
-function doom-emacs-sync
-    echo "Syncing Doom Emacs..."
-    doom sync
-    echo "Running Doom Emacs doctor..."
-    doom doctor
-end
-
-# Alias for doom-emacs-sync function
-alias sync 'doom-emacs-sync'
