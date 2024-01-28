a454ee223c976544f21ff3ef3ac3523546d6c48e
s-takoor <shashiduth.takoor@gmail.com>
2024-01-28 17:26:50 +0400

    refactor wezterm literate config

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -8,7 +8,7 @@ set -Ux FZF_DEFAULT_OPTS "\
 --ansi \
 --color='16,bg+:-1,gutter:-1,prompt:4,pointer:4,marker:6,border:4,label:4,header:italic'"
 
-set -Ux FZF_TMUX_OPTS "-p 55%,60%"
+set -Ux FZF_TMUX_OPTS "-p 70%,50%"
 
 set -Ux FZF_CTRL_R_OPTS "--border-label=' history ' \
 --prompt=' '"
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,59 +1,61 @@
 local wezterm = require("wezterm")
+
 return {
-  -- Updates
-  check_for_updates = true,
 
-  -- Scrollback
-  scrollback_lines = 3500,
+-- Updates
+check_for_updates = true,
 
-  -- Multiplexer
-  default_prog = { "/opt/homebrew/bin/tmux", "new-session", "-A", "-D", "-s", "main" },
+-- Scrollback
+scrollback_lines = 3500,
 
-  -- Colors
-  -- color_scheme = "Gruvbox Dark (Gogh)",
-  color_scheme = "Catppuccin Macchiato",
+-- Multiplexer
+default_prog = { "/opt/homebrew/bin/tmux", "new-session", "-A", "-D", "-s", "main" },
 
-  -- Cursor blink
-  default_cursor_style = "BlinkingBar",
+-- Colors
+-- color_scheme = "Gruvbox Dark (Gogh)",
+color_scheme = "Catppuccin Macchiato",
 
-  -- Font
-  font = wezterm.font_with_fallback({
-    { family = "JetBrainsMono NF", weight = "Medium", italic = false, },
-    { family = "SF Pro", weight = "Medium", italic = false, },
-  }),
-  font_size = 11.0,
+-- Cursor blink
+default_cursor_style = "BlinkingBar",
 
-  -- Blur
-  macos_window_background_blur = 20,
+-- Font
+font = wezterm.font_with_fallback({
+  { family = "JetBrainsMono NF", weight = "Medium", italic = false, },
+  { family = "SF Pro", weight = "Medium", italic = false, },
+}),
+font_size = 11.0,
 
-  inactive_pane_hsb = {
-    saturation = 0.24,
-    brightness = 0.5,
+-- Opacity
+window_background_opacity = 0.70,
+
+-- Blur
+macos_window_background_blur = 20,
+
+-- Padding
+window_padding = {
+  left = 40,
+  right = 40,
+  top = 40,
+  bottom = 40,
+},
+
+inactive_pane_hsb = {
+  saturation = 0.24,
+  brightness = 0.5,
+},
+
+-- Window decorations
+window_decorations = "RESIZE",
+
+-- Tab bar
+enable_tab_bar = false,
+
+-- Keybindings
+keys = {
+  {
+    key = "f",
+    mods = "CTRL",
+    action = wezterm.action.ToggleFullScreen,
   },
-
-  -- Padding
-  window_padding = {
-    left = 40,
-    right = 40,
-    top = 40,
-    bottom = 40,
-  },
-
-  -- Opacity
-  window_background_opacity = 0.70,
-
-  -- Window decorations
-  window_decorations = "RESIZE",
-
-  -- Tab bar
-  enable_tab_bar = false,
-
-  -- Keybindings
-  keys = {
-    {
-      key = "f",
-      mods = "CTRL",
-      action = wezterm.action.ToggleFullScreen,
-    },
-  },
+},
 }
